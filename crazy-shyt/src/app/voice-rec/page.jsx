8c16fe0aa714764b0d5b4d2494c0155b27933d0a
Harkirat <harkiratjaggi28@gmail.com>
2025-03-04 22:54:16 +0530

    voice messenger

---
+++ crazy-shyt/src/app/voice-rec/page.jsx
@@ -0,0 +1,11 @@
+import VoiceMessengerPreview from "@/components/VoiceMessengerPreview";
+
+const Page = () => {
+  return (
+    <div className="p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 pt-20 text-black">
+      <VoiceMessengerPreview/>
+    </div>
+  );
+};
+
+export default Page;
